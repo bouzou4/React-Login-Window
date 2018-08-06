d1b8a19c1bbf92938dfe1b901640c90f77acb57f
Adam Bouzourene <>
2018-08-06 14:53:40 -0400

    fix alert showing up on page load

--- src/components/LoginAlert.js
+++ src/components/LoginAlert.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
 
 export default function LoginAlert(props) {
-  if(props.showError) {
-    return (
-      <div className="log-form-alert alert alert-danger" role="alert">
-        <strong>Oh snap!</strong>
-        <ul>
-        {props.errorMsg.map(function(msg, ind) {
-          return (
-            <li key={ind}>{msg}</li>
-          );
-        })}
-        </ul>
-      </div>
-      );
-  }
+  return (
+    <div className="log-form-alert alert alert-danger" role="alert">
+      <strong>Oh snap!</strong>
+      <ul>
+      {props.errorMsg.map(function(msg, ind) {
+        return (
+          <li key={ind}>{msg}</li>
+        );
+      })}
+      </ul>
+    </div>
+  );
   return null;
 }
